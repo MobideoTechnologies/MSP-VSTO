ee3720f8fc5f9c1959a025f6082eab45fdfe5421
Tamirbs <tamirbensimon@gmail.com>
2023-03-12 19:01:32 +0200

    fix filtered files bug

--- ProjectAddIn3/Classes/MobideoImporter.cs
+++ ProjectAddIn3/Classes/MobideoImporter.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Office.Interop.MSProject;
+using Mobideo.Core;
 using Mobideo.Core.Extensions;
 using Mobideo.Integration.ProjectVSTOAddIn;
+using Newtonsoft.Json;
 using ProjectAddIn3.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -24,7 +26,7 @@ namespace ProjectAddIn3.Classes
         public async Task ImportFilesToMobideo(IEnumerable<SubProjectWrapper> selectedSubProjects, object importProgressBar)
         {
             Logger.Info("*** Start import files to mobideo");
-            var subProjectsFiles = CreateSubProjectFiles(selectedSubProjects);
+            var subProjectsFiles = CreateSubProjectFiles(selectedSubProjects, out var filesToDelete);
             var importProgress = importProgressBar as ProgressBar;
             importProgress.Style = ProgressBarStyle.Continuous;
             importProgress.Visible = true;
@@ -37,6 +39,8 @@ namespace ProjectAddIn3.Classes
                 {
                     Logger.Info("*** Uploading file {0} to customer files", subProjectFile.Item1);
                     await UploadFileToMobideo(subProjectFile.Item1, subProjectFile.Item2);
+                    subProjectFile.Item2.Close();
+                    subProjectFile.Item2.Dispose();
                     Logger.Info("*** Done Uploading file {0} to customer files", subProjectFile.Item1);
                 }
                 catch(Exception exception)
@@ -53,6 +57,7 @@ namespace ProjectAddIn3.Classes
 
             }
 
+            //filesToDelete.ForEach(file => File.Delete(file));
             importProgress.Visible= false;
 
             if (hasErrorOccured)
@@ -70,52 +75,30 @@ namespace ProjectAddIn3.Classes
             await mobideoFileUploader.UploadFile(filePath, stream, ConfigurationManager.AppSettings["CustomerFilesUploadFolderName"]);
         }
 
-        private List<Tuple<string,Stream>> CreateSubProjectFiles(IEnumerable<SubProjectWrapper> selectedSubProjects)
+        private List<Tuple<string,Stream>> CreateSubProjectFiles(IEnumerable<SubProjectWrapper> selectedSubProjects, out List<string> filesToDelete)
         {
             var subProjectFiles = new List<Tuple<string,Stream>>();
+            filesToDelete = new List<string>();
             var tempFolderPath = new Uri(Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase)).LocalPath + "\\TempFiles";
+            var application = Globals.ThisAddIn.Application;
             foreach (var subProject in selectedSubProjects)
             {
                 var currentSubProject = subProject.SubProjectLegacyObject;
                 var currentSubProjectPath = currentSubProject.Path;
-                var views = currentSubProject.SourceProject.Views;
-                foreach (var view in views)
-                {
-                    var castedView = view as View;
-                    if(!castedView.Name.CaseInsensitiveEquals("Gantt chart"))
-                    {
-                        castedView.Delete();
-                    }
-                }
-
-                //currentSubProject.Application.FilterClear();
-                //var taskFilters = currentSubProject.SourceProject.TaskFilters;
-                if (currentSubProject.SourceProject.AutoFilter)
-                {
-                    currentSubProject.SourceProject.AutoFilter = false;
-                }
-
-                //foreach (var filter in taskFilters)
-                //{
-                //    var currentFitler = filter as Filter;
-                //    if (!currentFitler.Name.CaseInsensitiveEquals("All Tasks") && !currentFitler.Name.CaseInsensitiveEquals("כל הפעילויות"))
-                //    {
-                //        currentFitler.Delete();
-                //    }
-
-                //}
-
-
-                var newUnfilteredFilePath  = string.Format("{0}\\{1}.{2}",tempFolderPath, Guid.NewGuid().ToString(), "mpp");
-                currentSubProject.SourceProject.SaveAs(newUnfilteredFilePath, PjFileFormat.pjMPP, Type.Missing, Type.Missing, Type.Missing, false, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
-
-                using (FileStream fileStream = new FileStream(newUnfilteredFilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                application.FileOpenEx(currentSubProjectPath);
+                application.FilterClear();
+                var newProjectFilePath = string.Format("{0}\\{1}.{2}", tempFolderPath, Guid.NewGuid().ToString(), "mpp");
+                application.FileSaveAs(newProjectFilePath);
+                using (FileStream fileStream = new FileStream(newProjectFilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
                 {
                     
                     var memoryStream = new MemoryStream();
                     fileStream.CopyTo(memoryStream);
                     subProjectFiles.Add(new Tuple<string, Stream>(currentSubProjectPath, memoryStream));
                 }
+
+                application.ActiveWindow.Close();
+                filesToDelete.Add(newProjectFilePath);
             }
 
             return subProjectFiles;
